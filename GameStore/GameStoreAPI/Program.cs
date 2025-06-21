02d02f0a248fb9b936eff09ed5a03b4141b7d30a
nooryass <m7md.safadi@hotmail.com>
2025-06-21 16:45:56 +0300

    registers all dependencies.

--- GameStore/GameStoreAPI/Program.cs
+++ GameStore/GameStoreAPI/Program.cs
@@ -1,5 +1,7 @@
 using GameStoreAPI.Data;
 using GameStoreAPI.Endpoints;
+using GameStoreAPI.Repositories;
+using GameStoreAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,14 @@ var connectionString = builder.Configuration.GetConnectionString("GameStoreConte
     throw new InvalidOperationException("Connection string 'GameStoreContext' not found.");
 
 builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
+// Register repositories
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+
+// Register services
+builder.Services.AddScoped<IGameService, GameService>();
+builder.Services.AddScoped<IGenreService, GenreService>();
 
 // Add CORS
 builder.Services.AddCors(options =>
